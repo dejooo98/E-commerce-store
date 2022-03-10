f831abbb033af0935bde831d3949a5d502518386
Darko <djukicdarko58@gmail.com>
2022-03-10 11:34:21 +0100

    upade hero section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
         <section class="hero">
           <div class="hero-container">
             <h1 class="text-slanted">Future Store</h1>
-            <h3 class="text-slanted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</h3>
+            <h2 class="text-slanted">Lorem Ipsum is simply dummy text of the printing</h2>
             <a class="btn hero-btn text-slanted">
               shop now
             </a>
