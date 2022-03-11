108e4e786ffce86ea9f228361ae18a1adce35c92
dejooo98 <dejan98vgd@gmail.com>
2022-03-11 16:10:02 +0100

    Pocetna stranica uradjeni stilovi i responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,34 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import ProductsList from '../components/ProductsList'
-import useFetch from './useFetch'
-
-
+import React from "react";
+import Footer from "../components/Footer";
+import ProductsList from "../components/ProductsList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  
-  const {data:furnitures, isPending, error} = useFetch('http://localhost:7000/furnitures');
-  
+  const {
+    data: furnitures,
+    isPending,
+    error,
+  } = useFetch("http://localhost:7000/furnitures");
 
   return (
     <div>
-        <section className="hero">
-          <div className="hero-container">
-            <h1 className="text-slanted">Future Store</h1>
-            <h2 className="text-slanted">Lorem Ipsum is simply dummy text of the printing</h2>
-            <a className="btn hero-btn text-slanted">
-              shop now 
-            </a>
-          </div>
-        </section>
-        {error && <div>{error}</div>}
-        {isPending && <div className='loader'>Loading...</div>}
-        {furnitures && <ProductsList furnitures={furnitures} title='All product'/>}
-        <Footer/>
+      <section className="hero">
+        <div className="hero-container">
+          <h1 className="text-slanted">Future Store</h1>
+          <h4 className="text-slanted">
+            Lorem Ipsum is simply dummy text of the printing
+          </h4>
+          <a className="btn hero-btn text-slanted">shop now</a>
+        </div>
+      </section>
+      {error && <div>{error}</div>}
+      {isPending && <div className="loader">Loading...</div>}
+      {furnitures && (
+        <ProductsList furnitures={furnitures} title="Our products" />
+      )}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
