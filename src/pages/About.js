06c05f86d86182aee1a167a6128cc1a32abff23e
dejooo98 <dejan98vgd@gmail.com>
2022-03-12 19:29:04 +0100

    Products page stilized and responsive

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const About = () => {
   return (
@@ -18,14 +19,15 @@ const About = () => {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat
           accusantium sapiente tempora sed dolore esse deserunt eaque excepturi,
           delectus error accusamus vel eligendi, omnis beatae. Quisquam, dicta.
-          Eos quod quisquam esse recusandae vitae neque dolore, obcaecati incidunt
-          sequi blanditiis est exercitationem molestiae delectus saepe odio
-          eligendi modi porro eaque in libero minus unde sapiente consectetur
-          architecto. Ullam rerum, nemo iste ex, eaque perspiciatis nisi, eum
-          totam velit saepe sed quos similique amet. Ex, voluptate accusamus
-          nesciunt totam vitae esse iste.
+          Eos quod quisquam esse recusandae vitae neque dolore, obcaecati
+          incidunt sequi blanditiis est exercitationem molestiae delectus saepe
+          odio eligendi modi porro eaque in libero minus unde sapiente
+          consectetur architecto. Ullam rerum, nemo iste ex, eaque perspiciatis
+          nisi, eum totam velit saepe sed quos similique amet. Ex, voluptate
+          accusamus nesciunt totam vitae esse iste.
         </p>
       </section>
+      <Footer />
     </>
   );
 };
