8976a957e80b63bbf28b2574121244385d96746f
dejooo98 <dejan98vgd@gmail.com>
2022-03-18 22:31:39 +0100

    Ispravljena greska prikazivanja slike

--- src/url/helper.js
+++ src/url/helper.js
@@ -1,7 +1,7 @@
 // flatten products
 export function flattenProducts(data) {
-  return data.map(item => {
-    let image = item.image.url;
+  return data.map((item) => {
+    let image = item.image;
     return { ...item, image };
   });
 }
